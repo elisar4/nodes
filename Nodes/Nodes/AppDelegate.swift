1e76f9b3b6819124be439be932d1b75c23e09e54
Vladimir <bobah1991@gmail.com>
2024-02-11 14:54:15 +0300

    add main package

--- Nodes/Nodes/AppDelegate.swift
+++ Nodes/Nodes/AppDelegate.swift
@@ -2,13 +2,14 @@
 //  Created by Vladimir Roganov on 11.02.2024
 
 import UIKit
+import App
 
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     let wnd = UIWindow()
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        wnd.rootViewController = ViewController()
+        wnd.rootViewController = AppController()
         wnd.makeKeyAndVisible()
         return true
     }
---
+++ modules/App/Sources/App/AppController.swift
@@ -0,0 +1,10 @@
+// AppController.swift
+
+import UIKit
+
+public class AppController: UIViewController {
+    public override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .blue
+    }
+}
