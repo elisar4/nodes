af44c5b8cbf506dc40034175bec52312a21efc8f
Vladimir <bobah1991@gmail.com>
2024-02-11 14:36:36 +0300

    clean up

--- Nodes/Nodes/ViewController.swift
+++ Nodes/Nodes/ViewController.swift
@@ -4,12 +4,8 @@
 import UIKit
 
 class ViewController: UIViewController {
-
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .blue
     }
-
-
 }
-
---
+++ Nodes/Nodes/main.swift
@@ -0,0 +1,10 @@
+//  main.swift
+//  Created by Vladimir Roganov on 11.02.2024
+
+import Foundation
+import UIKit
+
+exit(UIApplicationMain(CommandLine.argc,
+                       CommandLine.unsafeArgv,
+                       NSStringFromClass(UIApplication.self),
+                       NSStringFromClass(AppDelegate.self)))
