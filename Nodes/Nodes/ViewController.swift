d89622454dfaac804af561c7d199dd6cee19ddb5
Vladimir <bobah1991@gmail.com>
2024-02-11 14:16:18 +0300

    Empty project

---
+++ Nodes/Nodes/ViewController.swift
@@ -0,0 +1,15 @@
+//  ViewController.swift
+//  Created by Vladimir Roganov on 11.02.2024
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
