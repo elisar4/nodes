6eb69d02dadb2cc34fa4cf1dde99491092a7da3a
Vladimir <bobah1991@gmail.com>
2024-02-13 15:26:59 +0300

    nodes logic concept

--- modules/App/Sources/App/AppController.swift
+++ modules/App/Sources/App/AppController.swift
@@ -1,10 +1,21 @@
 // AppController.swift
 
 import UIKit
+import SwiftUI
 
 public class AppController: UIViewController {
+
+    let sui = UIHostingController(rootView: SUITest())
+
     public override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .blue
+        view.backgroundColor = UIColor(hue: 0.2, saturation: 0, brightness: 0.2, alpha: 1)
+        addChild(sui)
+        view.addSubview(sui.view)
+    }
+
+    public override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        sui.view.frame = view.bounds
     }
 }
---
+++ modules/App/Sources/App/View/SUITest.swift
@@ -0,0 +1,12 @@
+//  SUITest.swift
+//  Created by Vladimir Roganov on 13.02.2024
+
+import SwiftUI
+
+struct SUITest: View {
+
+    var body: some View {
+        Text("123")
+    }
+
+}
