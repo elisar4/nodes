f920d3818126d69c1de53bea5ad0150845bbbc6b
Vladimir <bobah1991@gmail.com>
2024-03-05 13:32:39 +0300

    implement workspace drag; closes #32

--- modules/App/Sources/App/View/MainView.swift
+++ modules/App/Sources/App/View/MainView.swift
@@ -6,40 +6,66 @@ import Logic
 
 struct MainView: View {
     @StateObject var controller = NodeLinkController()
+    @State private var offset: CGPoint = .zero
+    @State private var lastOffset: CGPoint = .zero
+
+    private var workspaceDragOffset: CGPoint {
+        return offset + lastOffset
+    }
 
     var body: some View {
         NavigationView {
             DebugView(controller: controller)
-            ZStack {
-                workspaceBackground
-                ForEach(controller.nodes, id: \.id) { node in
-                    NodeView(title: node.name, isSelected: controller.selection?.id == node.id) {
-                        node.build(controller: controller, id: node.id)
-                    } onInteraction: {
-                        controller.topNodeID = node.id
-                    }
-                    .zIndex(controller.topNodeID == node.id ? 1 : 0)
-                    .onTapGesture {
-                        controller.didTapNode(node)
-                    }
-                }
-                ForEach(controller.points) { element in
-                    LinkView(fromPoint: element.from, toPoint: element.to)
-                        .zIndex(2)
-                }
+
+            GeometryReader { geometry in
+                workspace(windowSize: geometry.frame(in: .local).size)
             }
-            .coordinateSpace(name: "ZStackMain")
-            .toolbar(content: { toolbarView })
-            .ignoresSafeArea(.keyboard)
+        }
+        .gesture(
+            DragGesture()
+                .onChanged { model in
+                    offset = .init(x: model.translation.width, y: model.translation.height)
+                }
+                .onEnded { model in
+                    lastOffset = lastOffset + .init(x: model.translation.width, y: model.translation.height)
+                    offset = .zero
+                }
+        )
+    }
+
+    private func workspace(windowSize: CGSize) -> some View {
+        ZStack {
+            WorkspaceBackground(windowSize: windowSize, workspaceOffset: workspaceDragOffset)
+                .onTapGesture {
+                    controller.didTapBackground()
+                }
+            ForEach(controller.nodes, id: \.id) { node in
+                makeNodeView(node)
+            }
+            ForEach(controller.points) { element in
+                LinkView(fromPoint: element.from, toPoint: element.to)
+                    .zIndex(2)
+            }
+        }
+        .position(workspaceDragOffset)
+        .coordinateSpace(name: "ZStackMain")
+        .toolbar(content: { toolbarView })
+        .ignoresSafeArea(.keyboard)
+        .onAppear {
+            lastOffset = .init(x: windowSize.width * 0.5, y: windowSize.height * 0.5)
         }
     }
 
-    var workspaceBackground: some View {
-        Image("bgPattern")
-            .resizable(resizingMode: .tile)
-            .onTapGesture {
-                controller.didTapBackground()
-            }
+    private func makeNodeView(_ node: any BaseNode) -> some View {
+        NodeView(title: node.name, isSelected: controller.selection?.id == node.id) {
+            node.build(controller: controller, id: node.id)
+        } onInteraction: {
+            controller.topNodeID = node.id
+        }
+        .zIndex(controller.topNodeID == node.id ? 1 : 0)
+        .onTapGesture {
+            controller.didTapNode(node)
+        }
     }
 
     @ViewBuilder
@@ -53,3 +79,4 @@ struct MainView: View {
         }
     }
 }
+
